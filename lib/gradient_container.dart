9b1e4077ed9c679b6ce2ade092afc7209ddf094b
Balaji-V-S <vsbrobot@gmail.com>
2023-09-09 19:20:22 +0530

    Bug Fixes

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-class GradientContainer extends StatelessWidget{
-  const GradientContainer({super.key}){
-    @override
-    Widget build(BuildContext context){
-      return  Container(
-        child:Center(child:Text('Hello World!',style:TextStyle(fontSize: 18),),),
-      );
-    }
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
